b696a1cde4151d42592806f51cb07f5fd139f505
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-06 17:00:36 +0300

    added object types

--- variables.ts
+++ variables.ts
@@ -3,3 +3,19 @@ const num: number = 1;
 const truthy: boolean = true;
 const nully: null = null;
 const nothing: undefined = undefined;
+
+// OBJECT TYPES
+class Color {}
+
+const myColor: Color = new Color();
+
+const person: { age: number } = {
+  age: 20
+};
+
+const strArr: string[] = ["1", "2", "3"];
+
+const myFunc: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+myFunc(3);
