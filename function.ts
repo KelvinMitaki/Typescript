7cf9f378c3492308ff9a48f88fcf426aad1d5d24
KelvinMitaki <mitakikelvin1@gmail.com>
2020-09-06 17:44:42 +0300

    added type annotations with function keyword

--- function.ts
+++ function.ts
@@ -1,3 +1,11 @@
-const myNum = (a: number, b: number): number => {
+const add = (a: number, b: number): number => {
   return a + b;
+};
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+const divide = function (a: number, b: number): number {
+  return a / b;
 };
